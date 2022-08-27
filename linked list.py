d9376284d7a113f69300173a1cd2576f97f2994f
Anay Pareek <anaypareek2003@gmail.com>
2022-08-27 20:10:29 +0530

    Add print in reverse

--- linked list.py
+++ linked list.py
@@ -1,14 +1,14 @@
 class Node:
-    def __init__(self, data):
+    def __init__(self, data, next=None):
         self.data = data
-        self.next = None
+        self.next = next
 
 class LinkedList:
     def __init__(self, *data):
-        nodes = [Node(i) for i in data]
-        self.start = nodes[0]
-        for i in range(len(nodes)-1):
-            nodes[i].next = nodes[i+1]
+        last_node = Node(data[-1])
+        for i in data[-2::-1]:
+            last_node = Node(i, last_node)
+        self.start = last_node
 
     @classmethod
     def from_list(cls, data_list: list):
@@ -25,3 +25,16 @@ class LinkedList:
                 break
             x = x.next
         return s
+
+    def print_in_reverse(self):
+        def reverse(node):
+            if node is not None:
+                reverse(node.next)
+                print(node.data, end=' -> ')
+        reverse(self.start)
+
+if __name__ == '__main__':
+    l = LinkedList('Hello', 1, True, {1, 2, 7, 5})
+    m = LinkedList.from_list([0, 1, 1, 2, 3, 5, 8, 13])
+    print(l, m, sep="\n")
+    m.print_in_reverse()
